f677e47bc4c3c460bf094a7540d7e16c16c6d3bb
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-18 23:03:25 +0200

    Lecture 2/9 Done.

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = () => {
+  return <Text style={style.textStyle}>This is the components screen</Text>;
+};
+
+const style = StyleSheet.create({
+  textStyle: {
+    fontSize: 30,
+  },
+});
+
+export default ComponentsScreen;
