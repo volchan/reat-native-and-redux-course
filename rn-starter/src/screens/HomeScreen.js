25af80936bc0f01b83601ccf23a57c5fb8812759
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 14:48:12 +0200

    Lecture 4/26 Done.

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +9,9 @@ const HomeScreen = () => {
         onPress={() => console.log("button pressed")}
         title="Go to Components demo"
       />
+      <TouchableOpacity onPress={() => console.log("List pressed")}>
+        <Text>Go to List demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
