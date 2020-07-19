c93339f5d05da839f56f3ae79bc3f992f672272a
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 13:47:13 +0200

    Lecture 3/16 Done.

---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+  return <Text>List Screen</Text>;
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default ListScreen;
