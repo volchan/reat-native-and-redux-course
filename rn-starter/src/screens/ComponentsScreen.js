e1b25323b477d8b6cde4e314a6727bc3f8e4894d
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 13:42:55 +0200

    Lecture 2/15 Done.

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={style.textStyle}>This is the components screen</Text>;
+  const name = "Stéphane";
+  return (
+    <View>
+      <Text style={style.titleStyle}>Getting started with React native!</Text>
+      <Text style={style.textStyle}>My name is {name}</Text>
+    </View>
+  );
 };
 
 const style = StyleSheet.create({
+  titleStyle: {
+    fontSize: 45,
+  },
   textStyle: {
-    fontSize: 30,
+    fontSize: 20,
   },
 });
 
