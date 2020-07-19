831805cd287a2d938722f4d898cf9b4f98df1c02
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 15:25:13 +0200

    Lecture 5/31 Done.

---
+++ rn-starter/src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Show image of forest</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
