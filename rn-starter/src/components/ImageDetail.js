ca36b30a0bb43cfcd1da570faa1b222430ded208
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 15:35:59 +0200

    Lecture 5/34 Done.

--- rn-starter/src/components/ImageDetail.js
+++ rn-starter/src/components/ImageDetail.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = ({ title }) => {
-  return <Text>{title}</Text>;
+  return (
+    <View>
+      <Image source={require("../../assets/beach.jpg")} />
+      <Text>{title}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({});
