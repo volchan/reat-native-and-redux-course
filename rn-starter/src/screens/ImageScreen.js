aa5381720ba6bad0383eb05358265fde89ea6b4c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-19 15:42:31 +0200

    Lecture 5/37 Done.

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -9,17 +9,17 @@ const ImageScreen = () => {
       <ImageDetail
         title="Forest"
         imageSource={require("../../assets/forest.jpg")}
-        score="9"
+        score={9}
       />
       <ImageDetail
         title="Beach"
         imageSource={require("../../assets/beach.jpg")}
-        score="7"
+        score={7}
       />
       <ImageDetail
         title="Mountain"
         imageSource={require("../../assets/mountain.jpg")}
-        score="4"
+        score={4}
       />
     </View>
   );
