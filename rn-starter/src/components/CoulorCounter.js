2a0d9705ab6ec01285c73bf0aad9433f2af4bfed
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-21 22:04:03 +0200

    Lecture 6/48 Done.

--- rn-starter/src/components/CoulorCounter.js
+++ rn-starter/src/components/CoulorCounter.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { View, StyleSheet, Button, Text } from "react-native";
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`}></Button>
-      <Button title={`Decrease ${color}`}></Button>
+      <Button title={`Increase ${color}`} onPress={() => onIncrease()}></Button>
+      <Button title={`Decrease ${color}`} onPress={() => onDecrease()}></Button>
     </View>
   );
 };
--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -4,11 +4,30 @@ import { View, StyleSheet, Button, Text } from "react-native";
 import ColorCounter from "../components/CoulorCounter";
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Green" />
-      <ColorCounter color="Bleu" />
+      <ColorCounter
+        color="Red"
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+      />
+      <ColorCounter
+        color="Green"
+        onIncrease={() => setGreen(green + 1)}
+        onDecrease={() => setGreen(green - 1)}
+      />
+      <ColorCounter
+        color="Bleu"
+        onIncrease={() => setBlue(blue + 1)}
+        onDecrease={() => setBlue(blue - 1)}
+      />
+      <Text>Red: {red}</Text>
+      <Text>Green: {green}</Text>
+      <Text>Blue: {blue}</Text>
     </View>
   );
 };
