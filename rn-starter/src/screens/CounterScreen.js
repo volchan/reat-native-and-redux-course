41810462bef7c898363d56be16282019943f38c8
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-21 23:10:22 +0200

    Lecture 6/58 Done.

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useReducer } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-
+const reducer = (state, action) => {
+  const { count } = state;
+  const { type, payload } = action
+  switch (type) {
+    case 'increment':
+      return { ...state, count: count + payload };
+    case 'decrement':
+      return { ...state, count: count - payload };
+    default:
+      return state;
+  }
+};
 
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
-  
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const { count } = state;
+
   return (
     <View>
-      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
-      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
-      <Text>Current Count: { counter }</Text>
+      <Button
+        title="Increase"
+        onPress={() => dispatch({ type: "increment", payload: 1 })}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => dispatch({ type: "decrement", payload: 1 })}
+      />
+      <Text>Current Count: {count}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({});
 
