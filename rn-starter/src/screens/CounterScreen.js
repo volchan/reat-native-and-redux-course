a3d0443d4f416d05f9d302b952aaa25388778b62
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-21 21:23:57 +0200

    Lecture 6/40 Done.

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+
+
 const CounterScreen = () => {
-  return <View>
-    <Text>Current Count:</Text>
-  </View>;
+  const [counter, setCounter] = useState(0);
+  
+  return (
+    <View>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
+      <Text>Current Count: { counter }</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
