35b60688c1dd07c6c48c660800cece9f179ca9d7
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-21 21:45:47 +0200

    Lecture 6/45 Done.

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
   const [colors, setColors] = useState([]);
   return (
     <View>
-      <Button title="Add a Color" onPress={() => setColors([...colors, randomRgb()])} />
-      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }}>
-      </View>
+      <Button
+        title="Add a Color"
+        onPress={() => setColors([...colors, randomRgb()])}
+      />
+      <FlatList
+        keyExtractor={item => item}
+        data={colors}
+        renderItem={({ item }) => (
+          <View
+            style={{ height: 100, width: 100, backgroundColor: item }}
+          />
+        )}
+      />
     </View>
   );
 };
 
 const randomRgb = () => {
-  const red = Math.floor(Math.random() * 256)
-  const green = Math.floor(Math.random() * 256)
-  const blue = Math.floor(Math.random() * 256)
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
 
   return `rgb(${red}, ${green}, ${blue})`;
-}
+};
 
 const styles = StyleSheet.create({});
 
