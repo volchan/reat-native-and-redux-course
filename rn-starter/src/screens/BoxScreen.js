1b7c6304848491a6ff871e99aa732043b63e7249
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-22 20:37:51 +0200

    Lecture 7/69 Done.

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, Text, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
@@ -13,11 +15,11 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: "black",
+    alignItems: "center",
   },
   textStyle: {
     borderWidth: 3,
     borderColor: "red",
-    margin: 20,
   }
 });
 
