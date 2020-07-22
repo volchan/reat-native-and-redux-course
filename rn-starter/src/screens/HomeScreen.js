1b03d24cf2ebf4abcc2c510cdae966b9b51a7ba6
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-22 20:06:26 +0200

    Lecture 6/60 Done.

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -29,6 +29,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Square")}
         title="Go to Square demo"
       />
+      <Button
+        onPress={() => navigation.navigate("Text")}
+        title="Go to Text demo"
+      />
     </View>
   );
 };
