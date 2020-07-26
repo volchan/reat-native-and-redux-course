4aacb9f083444efc61773e3be424dca87c444a32
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-26 21:44:49 +0200

    Lecture 8/89 Done.

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -3,12 +3,19 @@ import { View, Text, StyleSheet } from "react-native";
 
 const SearchBar = () => {
   return (
-    <View>
+    <View style={styles.background}>
       <Text>SearchBar</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+  }
+});
 
 export default SearchBar;
