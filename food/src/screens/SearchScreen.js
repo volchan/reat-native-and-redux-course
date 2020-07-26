e2d9d7b3eb5af35e8d933d508f3dd27d19a820ee
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-26 23:15:32 +0200

    Lecture 9/96 Done.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const res = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "Bordeaux",
+      },
+    });
+    setResults(res.data.businesses);
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log(`term "${term}" was submitted`)}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
       />
       <Text>SearchScreen</Text>
+      <Text>We have found {results.length} results.</Text>
     </View>
   );
 };
