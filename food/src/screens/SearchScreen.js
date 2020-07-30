8c2361cab3f8ff5f398eb7a474905eb512b512e2
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-07-30 19:55:15 +0200

    Lecture 10/102 Done.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-import yelp from "../api/yelp";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -14,7 +14,12 @@ const SearchScreen = () => {
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       {results.length > 0 ? (
-        <Text>We have found {results.length} results.</Text>
+        <View>
+          <Text>We have found {results.length} results.</Text>
+          <ResultsList title="Cost Effective" />
+          <ResultsList title="Bit Pricier" />
+          <ResultsList title="Big Spender" />
+        </View>
       ) : null}
     </View>
   );
