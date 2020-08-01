ecda22aabbed810458c2b31e63d65c7700d73294
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-01 11:54:29 +0200

    Lecture 11/113 Done.

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -5,7 +5,7 @@ import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
 import ResultsList from "../components/ResultsList";
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
@@ -19,9 +19,21 @@ const SearchScreen = () => {
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       {results.length > 0 ? (
         <ScrollView>
-          <ResultsList results={filterResultsByPrice("€€")} title="Cost Effective" />
-          <ResultsList results={filterResultsByPrice("€€€")} title="Bit Pricier" />
-          <ResultsList results={filterResultsByPrice("€€€€")} title="Big Spender" />
+          <ResultsList
+            results={filterResultsByPrice("€€")}
+            title="Cost Effective"
+            navigation={navigation}
+          />
+          <ResultsList
+            results={filterResultsByPrice("€€€")}
+            title="Bit Pricier"
+            navigation={navigation}
+          />
+          <ResultsList
+            results={filterResultsByPrice("€€€€")}
+            title="Big Spender"
+            navigation={navigation}
+          />
         </ScrollView>
       ) : null}
     </>
