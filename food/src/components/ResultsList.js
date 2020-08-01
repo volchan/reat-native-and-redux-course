1cc85d219fbc49251cb2174bb50ecf81ee224a44
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-01 12:19:44 +0200

    Lecture 11/118 Done.

--- food/src/components/ResultsList.js
+++ food/src/components/ResultsList.js
@@ -11,6 +11,9 @@ import {
 import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({ title, results, navigation }) => {
+  if (!results.length) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
