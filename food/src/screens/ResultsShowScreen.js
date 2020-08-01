24c93c7f155f1914728e359fe0607dde4eddb59e
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-01 12:09:57 +0200

    Lecture 11/116 Done.

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+import yelp from "../api/yelp";
+
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
+
+  const getResult = async (id) => {
+    const res = await yelp.get(`/${id}`);
+    console.log(res.data);
+    setResult(res.data);
+  };
+
+  useEffect(() => { getResult(id) }, [])
 
   return (
     <View>
