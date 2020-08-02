7a10cb9984f7d0838cb80b41c41e0a41d070aaad
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-02 21:43:00 +0200

    Lecture 12/127 Done.

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,7 +3,14 @@ import React from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={15}>{children}</BlogContext.Provider>;
+  const blogPosts = [
+    { title: "Blog Post #1" },
+    { title: "Blog Post #2" },
+    { title: "Blog Post #3" },
+    { title: "Blog Post #4" },
+  ]
+
+  return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>;
 };
 
 export default BlogContext;
