043757b49ac45bd258c5d8e9cdaf4568c6ab6158
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-02 22:49:55 +0200

    Lecture 12/134 Done.

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const {state, addBlogPost} = useContext(Context);
+  const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
@@ -13,12 +14,35 @@ const IndexScreen = () => {
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <FontAwesome5 name="trash-alt" style={styles.icon} />
+            </View>
+          );
+        }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+    color: "red",
+  },
+});
 
 export default IndexScreen;
