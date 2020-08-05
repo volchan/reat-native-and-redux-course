f0c8c43fc76d6ced2d74c5cb68691d4ba1574cbb
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-05 18:07:34 +0200

    Lecture 12/136 Done.

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -5,7 +5,7 @@ import { FontAwesome5 } from "@expo/vector-icons";
 import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
 
   return (
     <View>
@@ -13,12 +13,12 @@ const IndexScreen = () => {
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
-        keyExtractor={(blogPost) => blogPost.title}
+        keyExtractor={(blogPost) => blogPost.id.toString()}
         renderItem={({ item }) => {
           return (
             <View style={styles.row}>
               <Text style={styles.title}>{item.title}</Text>
-              <TouchableOpacity onPress={() => console.log(item.id)}>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
                 <FontAwesome5 name="trash-alt" style={styles.icon} />
               </TouchableOpacity>
             </View>
