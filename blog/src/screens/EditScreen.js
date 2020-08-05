25c779229711513fa2428048641fa25d72a44c20
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-05 22:24:48 +0200

    Lecture 12/149 Done.

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 import { Context } from "../context/BlogContext";
+import BlogPostFrom from "../components/BlogPostForm";
 
 const EditScreen = ({ navigation }) => {
   const { state } = useContext(Context);
@@ -12,49 +12,9 @@ const EditScreen = ({ navigation }) => {
   const [title, setTitle] = useState(blogPost.title);
   const [content, setContent] = useState(blogPost.content);
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Edit Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Add Blog Post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          });
-        }}
-      />
-    </View>
-  );
+  return <BlogPostFrom />;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-  },
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 10,
-    marginHorizontal: 10,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
