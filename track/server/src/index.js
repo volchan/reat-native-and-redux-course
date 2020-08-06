12ee76f3ce2c45c218801e22a3ac75681d5f83fa
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-06 20:14:12 +0200

    Lecture 14/171 Done.

--- track/server/src/index.js
+++ track/server/src/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const env = require("./config/env"); 
 
 const app = express();
+
+const mongoUri = env.MONGO_DB_URL;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => console.log("Connected to mongo instance"));
+mongoose.connection.on("error", (err) => console.error("Error connecting to mongo instance", err));
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
