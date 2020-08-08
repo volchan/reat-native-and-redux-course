64604dac7d915e2003b7f2ef6660b0df9c31345c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-08 18:22:24 +0200

    Lecture 14/183 Done.

--- track/server/src/models/User.js
+++ track/server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -11,5 +12,32 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", function(next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    })
+  })
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) return reject(err);
+      if (!isMatch) return reject(false);
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model("User", UserSchema);
