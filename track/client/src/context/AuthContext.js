dc4c51623ef894c37260f1d511e03feebc20b9f4
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-09 16:27:01 +0200

    Lecture 15/202 Done.

--- track/client/src/context/AuthContext.js
+++ track/client/src/context/AuthContext.js
@@ -9,8 +9,30 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to sign up that email and password
+    // if we sign up, modify our state, and say that we are authenticated
+    // if sign up fails, we probably need to reflect an error message
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // try to sign in
+    // handle success by updating state
+    // handle error by showing error message (somehow)
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    // just sign out
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
