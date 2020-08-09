c73ab4b58514c1d9efb88ba08a340138537748a1
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-09 17:07:23 +0200

    Add morgan logger to server

--- track/server/src/index.js
+++ track/server/src/index.js
@@ -4,6 +4,7 @@ require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const env = require("./config/env");
 const authRoutes = require("./routes/authRoutes");
@@ -11,6 +12,9 @@ const trackRoutes = require("./routes/trackRoutes");
 const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
+
+app.use(morgan("dev"));
+
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(trackRoutes);
@@ -21,8 +25,12 @@ mongoose.connect(mongoUri, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-mongoose.connection.on("connected", () => console.log("Connected to mongo instance"));
-mongoose.connection.on("error", (err) => console.error("Error connecting to mongo instance", err));
+mongoose.connection.on("connected", () =>
+  console.log("Connected to mongo instance")
+);
+mongoose.connection.on("error", (err) =>
+  console.error("Error connecting to mongo instance", err)
+);
 
 app.get("/", requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email}`);
