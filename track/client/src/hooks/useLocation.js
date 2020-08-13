7dc231207115b69f1f9db6a34d0435b49411bae2
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-13 21:29:07 +0200

    Lecture 15/239 Done.

---
+++ track/client/src/hooks/useLocation.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import {
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
+} from "expo-location";
+
+export default (callback) => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          callback(location);
+        }
+      );
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [ err ];
+};
--- track/client/src/screens/TrackCreateScreen.js
+++ track/client/src/screens/TrackCreateScreen.js
@@ -1,46 +1,17 @@
 import "../_mockLocation";
 
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import {
-  requestPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
 
 import Map from "../components/Map";
 import Container from "../components/Container";
 import { Context as LocationContext } from "../context/LocationContext";
+import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000,
-          distanceInterval: 10,
-        },
-        (location) => {
-          console.log(location.coords);
-          addLocation(location);
-        }
-      );
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  const [ err ] = useLocation(addLocation)
 
   return (
     <Container>
