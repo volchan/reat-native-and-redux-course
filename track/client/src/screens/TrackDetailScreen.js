bac64c49813ab6098048ebdafbdcee6ae47e49b9
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-15 17:03:15 +0200

    Lecture 15/260 Done.

--- track/client/src/screens/TrackDetailScreen.js
+++ track/client/src/screens/TrackDetailScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 
-const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>;
+import { Context as TrackContext } from "../context/TrackContext";
+
+const TrackDetailScreen = ({ navigation }) => {
+  const { state: tracks } = useContext(TrackContext);
+  const _id = navigation.getParam("_id");
+  const track = tracks.find((el) => el._id === _id);
+
+  return <Text style={styles.title}>{track.name}</Text>;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: { fontSize: 48, textAlign: "center" },
+});
 
 export default TrackDetailScreen;
--- track/client/src/screens/TrackListScreen.js
+++ track/client/src/screens/TrackListScreen.js
@@ -5,26 +5,44 @@ import { NavigationEvents } from "react-navigation";
 
 import Spacer from "../components/Spacer";
 import { Context as TrackContext } from "../context/TrackContext";
+import Container from "../components/Container";
 
 const TrackListScreen = ({ navigation }) => {
   const { state: tracks, fetchTracks } = useContext(TrackContext);
-  console.log(tracks);
+
   return (
-    <>
+    <Container>
       <NavigationEvents onWillFocus={fetchTracks} />
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
+      <Text style={styles.title}>TrackListScreen</Text>
       <Spacer />
       <FlatList
         data={tracks}
         keyExtractor={(item) => item._id}
-        renderItem={({ item }) => <TouchableOpacity>
-          <ListItem chevron title={item.name} />
-        </TouchableOpacity>}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("TrackDetail", { _id: item._id })
+            }
+          >
+            <ListItem chevron title={item.name} />
+          </TouchableOpacity>
+        )}
       />
-    </>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({});
+TrackListScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 48,
+    textAlign: "center",
+  },
+});
 
 export default TrackListScreen;
