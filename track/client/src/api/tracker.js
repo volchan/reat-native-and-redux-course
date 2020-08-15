cbb92bd23e4ccf97ca4b447b4692cfe563d9c1a1
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-15 16:16:03 +0200

    Lecture 15/256 Done.

--- track/client/src/api/tracker.js
+++ track/client/src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "http://95b93af81d56.eu.ngrok.io",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        Autorization: `Bearer ${token}`,
+      };
+    }
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
