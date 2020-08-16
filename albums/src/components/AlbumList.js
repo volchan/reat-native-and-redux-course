57b8f1d0c3bc307e026f526ac83d742bcc9c238d
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 16:04:34 +0200

    Lecture 21/289 Done.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 class AlbumList extends Component {
+  UNSAFE_componentWillMount() {
+    console.log('component album list will mount');
+  }
+
   render() {
     return (
       <View>
