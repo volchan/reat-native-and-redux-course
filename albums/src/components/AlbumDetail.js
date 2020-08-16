60ba9072a293f430a473ed3d5a9a7d80031eb98f
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 17:21:16 +0200

    Lecture 22/304 Done.

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -6,16 +6,29 @@ import CardSection from './CardSection';
 
 const AlbumList = ({album}) => {
   const {title, artist, image, thumbnail_image} = album;
-  const {header, thumbnail} = styles;
+  const {
+    header,
+    albumTitle,
+    albumCover,
+    albumCoverContainer,
+    artistName,
+    thumbnail,
+    thumbnailContainer,
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainer}>
           <Image style={thumbnail} source={{uri: thumbnail_image}} />
         </View>
         <View style={header}>
-          <Text>{title}</Text>
-          <Text>{artist}</Text>
+          <Text style={albumTitle}>{title}</Text>
+          <Text style={artistName}>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <View style={albumCoverContainer}>
+          <Image style={albumCover} source={{uri: image}} />
         </View>
       </CardSection>
     </Card>
@@ -27,10 +40,29 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around',
   },
+  albumTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  albumCover: {
+    borderRadius: 4,
+    flex: 1,
+  },
+  albumCoverContainer: {
+    flex: 1,
+    height: 400,
+  },
+  artistName: {
+    opacity: 0.5,
+  },
   thumbnail: {
     height: 50,
     width: 50,
     borderRadius: 4,
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginRight: 5,
   },
 };
