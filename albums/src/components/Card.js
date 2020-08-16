eac26e7695ee187c0994b67909939c828caebace
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 16:40:10 +0200

    Lecture 21/297 Done.

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import {View} from 'react-native';
 
-const Card = ({album}) => {
-  return <View></View>;
+const Card = () => {
+  return <View style={styles.container}></View>;
 };
 
-const styles = {};
+const styles = {
+  container: {
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#DDD',
+    borderBottom: 0,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+  },
+};
 
 export default Card;
