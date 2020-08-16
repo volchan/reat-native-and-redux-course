c03555c28876a22a202e3319702bee895af2e77c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 15:54:11 +0200

    Lecture 21/287 Done.

--- albums/index.js
+++ albums/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 
-import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+import Header from './src/components/Header';
 
-const App = () => <Header title="Albums" />;
+const App = () => {
+  return (
+    <>
+      <Header title="Albums" />
+      <AlbumList />
+    </>
+  );
+};
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List</Text>
+    </View>
+  );
+};
+
+const styles = {};
+
+export default AlbumList;
