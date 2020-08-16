86b083514756eb4212426f7db62fd6d2f511de88
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 15:58:37 +0200

    Lecture 21/288 Done.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
+}
 
 const styles = {};
 
