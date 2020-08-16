72c12a71fc06709f31f60da8186e102cff9bf041
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 15:42:50 +0200

    Lecture 20/284 Done.

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -13,7 +13,15 @@ const Header = () => {
 
 const styles = {
   view: {
-    backgroundColor: 'red',
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
   },
   text: {
     fontSize: 20,
