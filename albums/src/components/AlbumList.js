afffa0b15b24221cc1ed5ee12f2961bfb3c6def5
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-16 16:22:15 +0200

    Lecture 21/292 Done.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,14 +3,17 @@ import {Text, View} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {albums: []};
+
   async UNSAFE_componentWillMount() {
-    const res = await axios.get(
+    const {data} = await axios.get(
       'https://rallycoding.herokuapp.com/api/music_albums',
     );
-    console.log(res.data);
+    this.setState({albums: data});
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text>Album List</Text>
