c5e32e54125667fb0ef84bf8b729ee8ad3ebb8e8
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-23 17:20:06 +0200

    Lecture 23/317 Done.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 import {Text, SafeAreaView} from 'react-native';
+import firebase from 'firebase';
 
 import {Header} from './components/commons';
 
 class App extends Component {
+  UNSAFE_componentWillMount() {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: 'AIzaSyB-QKYJj8MsRww7KPFZDvCd8JSsuthcj0Y',
+        authDomain: 'react-native-auth-e2769.firebaseapp.com',
+        databaseURL: 'https://react-native-auth-e2769.firebaseio.com',
+        projectId: 'react-native-auth-e2769',
+        storageBucket: 'react-native-auth-e2769.appspot.com',
+        messagingSenderId: '750804124671',
+        appId: '1:750804124671:web:1ec1be30191422bb4a5dca',
+      });
+    }
+  }
+
   render() {
     return (
       <SafeAreaView>
