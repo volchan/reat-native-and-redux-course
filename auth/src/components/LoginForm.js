0192e0a2c20cbfd925adce755229334dd8be303d
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-08-23 17:32:20 +0200

    Lecture 23/319 Done.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {TextInput, View} from 'react-native';
 
 import {Button, Card, CardSection} from './commons';
 
@@ -7,8 +7,14 @@ class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <TextInput style={styles.input} />
+        </CardSection>
+
+        <CardSection>
+          <TextInput style={styles.input} />
+        </CardSection>
+
         <CardSection>
           <Button>Log In</Button>
         </CardSection>
@@ -16,5 +22,12 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  input: {
+    height: 20,
+    width: 100,
+  },
+};
 
 export default LoginForm;
