e8653faa8a6e286b55932a5282a3a1e3b32b5201
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2020-09-27 18:27:17 +0200

    Lecture 29/374 Done.

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,7 +1,11 @@
+import {EMAIL_CHANGED} from '../actions/types';
+
 const INITIAL_STATE = {email: ''};
 
 export default (state = INITIAL_STATE, {type, payload}) => {
   switch (type) {
+    case EMAIL_CHANGED:
+      return {...state, email: payload};
     default:
       return state;
   }
